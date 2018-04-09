3f2672a0235c2a0b783df86340d5a435f41bcc25
戴运鹏 <dyp@weyee.com>
2018-04-09 14:14:28 +0800

    model转场角对角动画

--- CustomTransition_Demo/CustomTransition_Demo/PresentationTransition/ModelTrasitionAnimator.m
+++ CustomTransition_Demo/CustomTransition_Demo/PresentationTransition/ModelTrasitionAnimator.m
@@ -69,63 +69,110 @@
 #pragma mark -- 生成动画
 
 //在这里，如果presentingVC被nav持有或者不持有的话，fromVC会不同，但是最终动画的效果是一样的，都可以按照fromVC就是presentingvc的思路来处理动画、
+//- (void)performPresentAnimationWithContext:(id<UIViewControllerContextTransitioning>)transitionContext
+//{
+//    //可以通过上下文取出转场涉及到的两个控制器，以及两个控制器的view
+//    //fromVC就是presentingVC
+//    UIViewController *fromVC = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
+//    UIViewController *toVC = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
+//    //是否使用手势
+//    //转场结束，containerView会将fromVC.view移除，所以要想在转场结束后，仍然看到fromVC.view，用截图方法
+//    UIView *captureView = [fromVC.view snapshotViewAfterScreenUpdates:NO];
+//    captureView.frame = fromVC.view.frame;
+//    fromVC.view.hidden = YES;
+//    //转场过程中的容器view。两个控制器的view都加在这个view中，然后通过移动其坐标，实现转场
+//    UIView *containerView = [transitionContext containerView];
+//    //在模态Presentation中，fromVC.view是已经加在containerView上的。但是toVC.view需要我们手动加，如果用fromVC.view的截图替代fromVC.view的话，在这里也要手动加到containerView上。
+//    [containerView addSubview:captureView];
+//    [containerView addSubview:toVC.view];
+//
+//    toVC.view.frame = CGRectMake(0, containerView.frame.size.height, containerView.frame.size.width, 500);
+//    [UIView animateWithDuration:[self transitionDuration:transitionContext] delay:0 usingSpringWithDamping:0.55 initialSpringVelocity:1/0.44 options:0 animations:^{
+//        //将toVC.view从containerview的下边移动到能见范围内
+//        toVC.view.transform = CGAffineTransformMakeTranslation(0, -500);
+//        captureView.transform = CGAffineTransformMakeScale(0.9, 0.9);
+//
+//    } completion:^(BOOL finished) {
+//        //转场结束后，要将结果告诉UIKit
+//        [transitionContext completeTransition:![transitionContext transitionWasCancelled]];
+//        if ([transitionContext transitionWasCancelled]) {
+//            fromVC.view.hidden = NO;
+//            [captureView removeFromSuperview];
+//        }
+//    }];
+//
+//}
+
+//- (void)performDismissAnimationWithContext:(id<UIViewControllerContextTransitioning>)transitionContext
+//{
+//    //被presentation出来的控制器
+//    UIViewController *fromVC = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
+//    UIViewController *toVC = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
+//    UIView *containerView = [transitionContext containerView];
+//    NSArray *subviewsArray = containerView.subviews;
+//    UIView *captureView = subviewsArray[MIN(subviewsArray.count, MAX(0, subviewsArray.count - 2))];
+//
+//    [UIView animateWithDuration:[self transitionDuration:transitionContext] animations:^{
+//        //因为present的时候都是使用的transform，这里的动画只需要将transform恢复就可以了
+//        fromVC.view.transform = CGAffineTransformIdentity;
+//        captureView.transform = CGAffineTransformIdentity;
+//    } completion:^(BOOL finished) {
+//        if ([transitionContext transitionWasCancelled]) {
+//            //失败了接标记失败
+//            [transitionContext completeTransition:NO];
+//        }else{
+//            //如果成功了，我们需要标记成功，同时让toVC显示出来，然后移除截图视图，
+//            [transitionContext completeTransition:YES];
+//            toVC.view.hidden = NO;
+//
+//            [captureView removeFromSuperview];
+//        }
+//    }];
+//}
+
 - (void)performPresentAnimationWithContext:(id<UIViewControllerContextTransitioning>)transitionContext
 {
-    //可以通过上下文取出转场涉及到的两个控制器，以及两个控制器的view
-    //fromVC就是presentingVC
     UIViewController *fromVC = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
     UIViewController *toVC = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
-    //是否使用手势
-    //转场结束，containerView会将fromVC.view移除，所以要想在转场结束后，仍然看到fromVC.view，用截图方法
-    UIView *captureView = [fromVC.view snapshotViewAfterScreenUpdates:NO];
-    captureView.frame = fromVC.view.frame;
-    fromVC.view.hidden = YES;
-    //转场过程中的容器view。两个控制器的view都加在这个view中，然后通过移动其坐标，实现转场
     UIView *containerView = [transitionContext containerView];
-    //在模态Presentation中，fromVC.view是已经加在containerView上的。但是toVC.view需要我们手动加，如果用fromVC.view的截图替代fromVC.view的话，在这里也要手动加到containerView上。
-    [containerView addSubview:captureView];
-    [containerView addSubview:toVC.view];
+    UIView *formView =  fromVC.view;
+    UIView *toView = toVC.view;
+    [containerView addSubview:toView];
+    toView.frame = CGRectMake(containerView.frame.size.width, containerView.frame.size.height, containerView.frame.size.width, containerView.frame.size.height);
     
-    toVC.view.frame = CGRectMake(0, containerView.frame.size.height, containerView.frame.size.width, 500);
-    [UIView animateWithDuration:[self transitionDuration:transitionContext] delay:0 usingSpringWithDamping:0.55 initialSpringVelocity:1/0.44 options:0 animations:^{
-        //将toVC.view从containerview的下边移动到能见范围内
-        toVC.view.transform = CGAffineTransformMakeTranslation(0, -500);
-        captureView.transform = CGAffineTransformMakeScale(0.9, 0.9);
-        
+    [UIView animateWithDuration:2 animations:^{
+        formView.transform = CGAffineTransformMakeTranslation(-containerView.frame.size.width, -containerView.frame.size.height);
+        toView.transform = CGAffineTransformMakeTranslation(-containerView.frame.size.width, -containerView.frame.size.height);
     } completion:^(BOOL finished) {
-        //转场结束后，要将结果告诉UIKit
-        [transitionContext completeTransition:![transitionContext transitionWasCancelled]];
         if ([transitionContext transitionWasCancelled]) {
-            fromVC.view.hidden = NO;
-            [captureView removeFromSuperview];
+  
+            [transitionContext completeTransition:NO];
+        }else{
+            [transitionContext completeTransition:YES];
         }
     }];
-    
 }
 
 - (void)performDismissAnimationWithContext:(id<UIViewControllerContextTransitioning>)transitionContext
 {
-    //被presentation出来的控制器
     UIViewController *fromVC = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
     UIViewController *toVC = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
     UIView *containerView = [transitionContext containerView];
-    NSArray *subviewsArray = containerView.subviews;
-    UIView *captureView = subviewsArray[MIN(subviewsArray.count, MAX(0, subviewsArray.count - 2))];
-
-    [UIView animateWithDuration:[self transitionDuration:transitionContext] animations:^{
-        //因为present的时候都是使用的transform，这里的动画只需要将transform恢复就可以了
-        fromVC.view.transform = CGAffineTransformIdentity;
-        captureView.transform = CGAffineTransformIdentity;
+    UIView *formView =  fromVC.view;
+    UIView *toView = toVC.view;
+    [containerView addSubview:toView];
+    toView.frame = CGRectMake(-containerView.frame.size.width, -containerView.frame.size.height, containerView.frame.size.width, containerView.frame.size.height);
+    
+    [UIView animateWithDuration:2 animations:^{
+        formView.transform  = CGAffineTransformIdentity;
+        toView.transform = CGAffineTransformIdentity;
     } completion:^(BOOL finished) {
+   
         if ([transitionContext transitionWasCancelled]) {
-            //失败了接标记失败
+            
             [transitionContext completeTransition:NO];
         }else{
-            //如果成功了，我们需要标记成功，同时让toVC显示出来，然后移除截图视图，
             [transitionContext completeTransition:YES];
-            toVC.view.hidden = NO;
-            
-            [captureView removeFromSuperview];
         }
     }];
 }
