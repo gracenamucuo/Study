fd0286850429fa23710c2b0896b3db5f17505eac
戴运鹏 <dyp@weyee.com>
2018-05-11 11:51:22 +0800

    设计模式demo

---
+++ Design_Demo/Design_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Design_Demo
+//
+//  Created by 戴运鹏 on 2018/5/11.
+//  Copyright © 2018年 戴运鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
