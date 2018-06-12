d4032821a1986c021269199ad72a63cb702cb02f
戴运鹏 <dyp@weyee.com>
2018-06-12 22:12:33 +0800

    中介者

---
+++ Design_Demo/Design_Demo/中介者模式/MediatorPatternController.m
@@ -0,0 +1,25 @@
+//
+//  MediatorPatternController.m
+//  Design_Demo
+//
+//  Created by 戴运鹏 on 2018/6/10.
+//  Copyright © 2018年 戴运鹏. All rights reserved.
+//
+
+#import "MediatorPatternController.h"
+
+@interface MediatorPatternController ()
+
+@end
+
+@implementation MediatorPatternController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
+
+
