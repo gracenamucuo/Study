6b8696a8c2e4d628900a2adad02c060db3f59352
戴运鹏 <dyp@weyee.com>
2019-01-20 22:29:31 +0800

    设计原则

---
+++ SwiftExercise/设计原则/OOD-Principles.swift
@@ -0,0 +1,10 @@
+//
+//  OOD-Principles.swift
+//  设计原则
+//
+//  Created by 戴运鹏 on 2019/1/20.
+//  Copyright © 2019 戴运鹏. All rights reserved.
+//
+
+import Foundation
+//开闭原则：具体的实现方式:继承，或者用Swift的协议
---
+++ SwiftExercise/设计原则/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  设计原则
+//
+//  Created by 戴运鹏 on 2019/1/20.
+//  Copyright © 2019 戴运鹏. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
